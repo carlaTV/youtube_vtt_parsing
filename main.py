f5fa23b94939ed409f28f0f446a1d92aa5e0363c
Carla <ctenven@gmail.com>
2019-03-29 09:42:31 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,47 +1,70 @@
 from file_preprocessor import files_preprocessing as fp
-from file_preprocessor import remove_stopwords as stpw
+from file_preprocessor import download_youtube as dy
 from vtt_manager import vtt_manager as vttm
 from file_manager import file_manager as fm
+import shutil
+import os
 
 
 class Pipeline:
-    def __init__(self):
+    def __init__(self, lang):
+        self.lang = lang
         self.data = None
         self.data_list = None
-        self.ids = None
+        self.ids = []
+        self.manager = vttm.VTTManager()
+
+    def download_videos(self):
+        downloader = dy.YoutubeDownloader()
+        self.ids = downloader.read_ids()
+        downloader.download_videos()
 
     def pre_process(self):
-        downloader = fp.DownloadsManager()
-        downloader.move_videos_to_dir()
+        downloads_manager = fp.DownloadsManager()
+        for id in self.ids:
+            downloads_manager.remove_heading(id, self.lang)
 
     def process(self):
-        manager = vttm.VTTManager()
-        self.ids = manager.get_ids()
-        self.data, self.data_list = manager.parse_vtt()
+        self.data, self.data_list = self.manager.parse_vtt(self.ids, self.lang)
+        # for id in self.ids:
+        #     try:
+        #         data = self.data_list[id]
+        #         for d in data:
+        #             print(d)
+        #     except KeyError:
+        #         continue
 
     def save_results(self):
         file_manager = fm.FileManager()
 
-        # for id in self.ids:
-        #     file_manager.write_results_opening(id)
-        #     file_manager.write_results_content(id, self.data_list)
-        #     file_manager.write_end_file(id)
+        for id in self.ids:
+            file_manager.write_results_opening(id)
+            file_manager.write_results_content(id, self.data_list)
+            file_manager.write_end_file(id)
+            self.delete_files(id)
 
-        for num, chunk in enumerate(self.data):
-            file_manager.write_results_opening(self.ids[num])
-            file_manager.write_results_content_data(self.ids[num], chunk)
-            file_manager.write_end_file(self.ids[num])
+        # for num, chunk in enumerate(self.data):
+        #     file_manager.write_results_opening(self.ids[num])
+        #     file_manager.write_results_content_data(self.ids[num], chunk)
+        #     file_manager.write_end_file(self.ids[num])
+
+    @staticmethod
+    def delete_files(id):
+        upper_path = 'resources/youtube_downloads'
+        path = 'resources/youtube_downloads/{}'
+        path = path.format(id)
+        if os.path.exists(path):
+            shutil.rmtree(path)
+        if not os.listdir(upper_path):
+            shutil.rmtree(upper_path)
 
 
 def main():
-    pipeline = Pipeline()
-
-    preprocessing = input('Do you want to preprocess the files? [Y/n]\n')
-    if preprocessing.upper() == 'y':
-        pipeline.pre_process()
-    else:
-        pipeline.process()
-        pipeline.save_results()
+    pipeline = Pipeline('es')
+    pipeline.download_videos()
+    pipeline.pre_process()
+    pipeline.process()
+    pipeline.save_results()
 
 
 if __name__ == '__main__':
